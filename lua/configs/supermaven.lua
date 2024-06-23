eb60b6b2396e719abae60156e6d22fe60aeaa3f8
James Oppel <jamesoppel@gmail.com>
2024-06-23 20:40:19 +0900

    (re)init

---
+++ lua/configs/supermaven.lua
@@ -0,0 +1,16 @@
+local supermaven = require("supermaven-nvim")
+
+supermaven.setup({
+  keymaps = {
+    accept_suggestion = "<c-y>",
+    clear_suggestion = "<c-]>",
+    accept_word = "<c-j>",
+  },
+  color = {
+    suggestion_color = "#ff2c2c",
+    cterm = 244,
+  },
+  log_level = "info", -- set to "off" to disable logging completely
+  disable_inline_completion = false, -- disables inline completion for use with cmp
+  disable_keymaps = false -- disables built in keymaps for more manual control
+})
---
+++ lua/plugins/init.lua
@@ -0,0 +1,78 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server",
+        "stylua",
+  			"html-lsp",
+        "css-lsp",
+        "prettierd",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        "eslint-lsp",
+  		},
+  	},
+  },
+  {
+    "supermaven-inc/supermaven-nvim",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "lua",
+    },
+    config = function()
+      require "configs.supermaven"
+    end,
+  },
+  {
+  	"nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "configs.null-ls"
+    end
+  },
+  {
+  	"windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+      },
+    },
+  },
+}
